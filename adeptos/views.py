3bc60921b7023b8f696c2194f81f73dc580db179
Rodrigo Bezerra <rodbn87@gmail.com>
2023-11-17 10:01:40 +0000

    Implantando o botão para gerar um relatório pdf do Inquérito

--- adeptos/views.py
+++ adeptos/views.py
@@ -1,7 +1,13 @@
 import tkinter as tk
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.ticker import MaxNLocator
 from PIL import Image, ImageTk
 import numpy as np
+import os
+from reportlab.pdfgen import canvas
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from reportlab.lib.pagesizes import letter
 
 class AdeptosView:
     def __init__(self, master, adeptos_controller, cadastrar_callback):
@@ -73,6 +79,10 @@ class AdeptosView:
         self.btn_plot_barras_empilhadas = tk.Button(frame_botoes, text="Adeptos por Distrito", command=self.plot_barras_empilhadas)
         self.btn_plot_barras_empilhadas.grid(row=3, column=0, padx=5, pady=5, sticky="N")
 
+        # Adiciona o botão para exportar o relatório em PDF
+        self.btn_exportar_relatorio = tk.Button(frame_botoes, text="Exportar Relatório PDF", command=self.exportar_relatorio_pdf)
+        self.btn_exportar_relatorio.grid(row=4, column=0, padx=5, pady=5, sticky="N")
+
     def get_adepto_info(self):
         nome = self.entry_nome.get()
         idade = self.entry_idade.get()
@@ -114,7 +124,7 @@ class AdeptosView:
 
         return frame
 
-    def plot_barras(self):
+    """def plot_barras(self):
         dados = self.adeptos_controller.get_dados_para_grafico()
 
         soma_por_equipa = {'Benfica': 0, 'Sporting': 0, 'Porto': 0, 'Outros': 0}
@@ -129,6 +139,7 @@ class AdeptosView:
 
         cores = {'Benfica': 'red', 'Sporting': 'green', 'Porto': 'blue', 'Outros': 'orange'}
 
+        
         fig, ax = plt.subplots()
         bars = ax.bar(equipas, quantidades, color=[cores.get(equipe, 'gray') for equipe in equipas])
 
@@ -224,3 +235,166 @@ class AdeptosView:
         plt.title('Quantidade de Adeptos por Distrito por Equipa')
         plt.legend()
         plt.show()
+    """
+    def exportar_relatorio_pdf(self):
+    # Nome do arquivo PDF
+        pdf_filename = "Relatório de Inquérito de Adeptos.pdf"
+
+        # Caminho completo do arquivo PDF
+        pdf_path = os.path.join(os.path.expanduser("~"), "Downloads", pdf_filename)
+
+        # Se o arquivo já existe, adiciona um número ao nome do arquivo
+        count = 1
+        while os.path.exists(pdf_path):
+            pdf_filename = f"Relatório de Inquérito de Adeptos_{count}.pdf"
+            pdf_path = os.path.join(os.path.expanduser("~"), "Downloads", pdf_filename)
+            count += 1
+
+        # Cria um objeto PdfPages para salvar os gráficos no PDF
+        with PdfPages(pdf_path) as pdf:
+            # Adiciona um cabeçalho ao PDF
+            self.adicionar_cabecalho(pdf)
+
+            # Gera e salva o gráfico de barras no PDF
+            self.plot_barras(pdf)
+
+            # Gera e salva o gráfico de linha no PDF
+            self.plot_linha(pdf)
+
+            # Gera e salva o gráfico de barras empilhadas no PDF
+            self.plot_barras_empilhadas(pdf)
+
+        # Mensagem de sucesso
+        tk.messagebox.showinfo("Relatório Exportado", f"O relatório foi exportado com sucesso para {pdf_filename}")
+
+    def adicionar_cabecalho(self, pdf):
+        # Adiciona um cabeçalho ao PDF
+        fig, ax = plt.subplots()
+        ax.text(0.5, 0.95, 'Relatório de Inquérito de Adeptos', ha='center', va='center', fontsize=16)
+        ax.axis('off')  # Desativa os eixos para um cabeçalho limpo
+        pdf.savefig()
+        plt.close()
+
+    def plot_barras(self, pdf=None):
+        dados = self.adeptos_controller.get_dados_para_grafico()
+
+        soma_por_equipa = {'Benfica': 0, 'Sporting': 0, 'Porto': 0, 'Outros': 0}
+
+        for row in dados:
+            equipe = row[0]
+            quantidade = row[2]
+            soma_por_equipa[equipe] += quantidade
+
+        equipas = list(soma_por_equipa.keys())
+        quantidades = list(soma_por_equipa.values())
+
+        cores = {'Benfica': 'red', 'Sporting': 'green', 'Porto': 'blue', 'Outros': 'orange'}
+
+        fig, ax = plt.subplots()
+        bars = ax.bar(equipas, quantidades, color=[cores.get(equipe, 'gray') for equipe in equipas])
+
+        for bar in bars:
+            yval = bar.get_height()
+            plt.text(bar.get_x() + bar.get_width() / 2, yval, round(yval, 2), ha='center', va='bottom')
+
+        plt.xlabel('Equipa')
+        plt.ylabel('Quantidade de Adeptos')
+        plt.title('Quantidade de Adeptos por Equipa')
+
+        # Salva o gráfico no PDF
+        if pdf is None:
+            plt.show()
+        else:
+        # Se pdf for fornecido, salva o gráfico no PDF
+            pdf.savefig()
+            plt.close()
+
+    def plot_linha(self, pdf=None):
+        dados = self.adeptos_controller.get_dados_para_grafico()
+
+        media_idade_por_equipa = {'Benfica': 0, 'Sporting': 0, 'Porto': 0, 'Outros': 0}
+
+        contagem_por_equipa = {'Benfica': 0, 'Sporting': 0, 'Porto': 0, 'Outros': 0}
+
+        for row in dados:
+            equipe = row[0]
+            quantidade = row[2]
+            idade_media = row[3]
+
+            media_idade_por_equipa[equipe] += idade_media * quantidade
+            contagem_por_equipa[equipe] += quantidade
+
+        for equipe in media_idade_por_equipa:
+            if contagem_por_equipa[equipe] > 0:
+                media_idade_por_equipa[equipe] /= contagem_por_equipa[equipe]
+
+        equipas = list(media_idade_por_equipa.keys())
+        medias_idade = list(media_idade_por_equipa.values())
+
+        cores = {'Benfica': 'red', 'Sporting': 'green', 'Porto': 'blue', 'Outros': 'orange'}
+
+        fig, ax = plt.subplots()
+        ax.plot(equipas, medias_idade, marker='o')
+
+        for i, txt in enumerate(medias_idade):
+            ax.annotate(round(txt, 1), (equipas[i], medias_idade[i]))
+
+        plt.xlabel('Equipa')
+        plt.ylabel('Média de Idade')
+        plt.title('Média de Idade dos Adeptos por Equipa')
+
+        # Salva o gráfico no PDF
+        if pdf is None:
+            plt.show()
+        else:
+        # Se pdf for fornecido, salva o gráfico no PDF
+            pdf.savefig()
+            plt.close()
+
+    def plot_barras_empilhadas(self, pdf=None):
+        dados = self.adeptos_controller.get_dados_para_grafico()
+        distritos = sorted(list(set([row[1] for row in dados])))
+        equipas = ['Benfica', 'Sporting', 'Porto', 'Outros']
+
+        quantidades_por_equipe_e_distrito = np.zeros((len(equipas), len(distritos)))
+
+        for row in dados:
+            equipe = row[0]
+            distrito = row[1]
+            quantidade = row[2]
+            indice_equipe = equipas.index(equipe)
+            indice_distrito = distritos.index(distrito)
+            quantidades_por_equipe_e_distrito[indice_equipe, indice_distrito] += quantidade
+
+        soma_por_distrito = np.sum(quantidades_por_equipe_e_distrito, axis=0)
+
+        distritos_ordenados = [distrito for _, distrito in sorted(zip(soma_por_distrito, distritos), reverse=True)]
+
+        cores = {'Benfica': 'red', 'Sporting': 'green', 'Porto': 'blue', 'Outros': 'orange'}
+
+        fig, ax = plt.subplots()
+        bottom = np.zeros(len(distritos_ordenados))
+
+        for i, equipe in enumerate(equipas):
+            barra = ax.bar(distritos_ordenados, [quantidades_por_equipe_e_distrito[i, distritos.index(distrito)] for distrito in distritos_ordenados],
+                           color=cores.get(equipe, 'gray'), label=equipe, bottom=bottom)
+            bottom += [quantidades_por_equipe_e_distrito[i, distritos.index(distrito)] for distrito in distritos_ordenados]
+
+            for rect, quantidade in zip(barra, [quantidades_por_equipe_e_distrito[i, distritos.index(distrito)] for distrito in distritos_ordenados]):
+                if quantidade > 0:
+                    height = rect.get_height()
+                    width = rect.get_width()
+                    ax.text(rect.get_x() + width / 2, rect.get_y() + height / 2, f'{int(quantidade)}', ha='center', va='center', color='white')
+
+        plt.xlabel('Distrito')
+        plt.ylabel('Quantidade de Adeptos')
+        plt.title('Quantidade de Adeptos por Distrito por Equipa')
+        plt.legend()
+
+        # Salva o gráfico no PDF
+        if pdf is None:
+            plt.show()
+        else:
+        # Se pdf for fornecido, salva o gráfico no PDF
+            pdf.savefig()
+            plt.close()
